ba69c93d7942ed152b51d57318e70ae12e42f062
Caio Machado <caioamsul@gmail.com>
2021-03-04 10:44:18 -0300

    OnClick - Botão

--- app/src/main/java/com/example/gasool_completo/MainActivity.kt
+++ app/src/main/java/com/example/gasool_completo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gasool_completo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button : AppCompatButton = findViewById(R.id.button_calculate)
+        button.setOnClickListener{
+            Toast.makeText(this, "Cálculo Feito", Toast.LENGTH_LONG). show()
+        }
+
     }
 }
