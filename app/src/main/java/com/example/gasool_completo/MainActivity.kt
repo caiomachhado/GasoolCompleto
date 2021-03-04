1f31ca432be1f06c52a293806b7604744ccb689d
Caio Machado <caioamsul@gmail.com>
2021-03-04 12:24:06 -0300

    Inserção de Visualização no Click

--- app/src/main/java/com/example/gasool_completo/MainActivity.kt
+++ app/src/main/java/com/example/gasool_completo/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.TextViewCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +13,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val button : AppCompatButton = findViewById(R.id.button_calculate)
+        val textViewResult : AppCompatTextView = findViewById(R.id.text_view_result)
+
         button.setOnClickListener{
             Toast.makeText(this, "Cálculo Feito", Toast.LENGTH_LONG). show()
+            textViewResult.text = "O cálculo foi armazenado."
         }
 
     }
