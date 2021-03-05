770a0f193ff71d08dc3074a4ff2c79757f82eca3
Caio Machado <caioamsul@gmail.com>
2021-03-05 08:18:54 -0300

    Activity Design of Expenditure Created

--- app/src/main/java/com/example/gasool_completo/CombustivelActivity.kt
+++ app/src/main/java/com/example/gasool_completo/CombustivelActivity.kt
@@ -2,10 +2,15 @@ package com.example.gasool_completo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
 
 class CombustivelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_combustivel)
+
+        // val button : AppCompatButton = findViewById(R.id.button_result_consume)
+
+
     }
 }
