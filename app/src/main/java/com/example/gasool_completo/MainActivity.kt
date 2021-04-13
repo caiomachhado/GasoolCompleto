4459e0582cfd5a4fa5a573f05be2e0cd553d5021
Caio Machado <caioamsul@gmail.com>
2021-04-13 18:53:09 -0300

    Feito

--- app/src/main/java/com/example/gasool_completo/MainActivity.kt
+++ app/src/main/java/com/example/gasool_completo/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.gasool_completo
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.view.View
-import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
-import androidx.core.widget.TextViewCompat
-import androidx.
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,9 +22,12 @@ class MainActivity : AppCompatActivity() {
                 buttonCalculate.setOnClickListener{
                     val alcoholPrice = editTextAlcohol.text.toString()
                     val oilPrice = editTextOil.text.toString()
-                    val result = alcoholPrice.toDouble()
-
-                }
+                    val result = alcoholPrice.toDouble()/oilPrice.toDouble()
+                    if(result < 0.7){
+                        textViewResult.text = "Alcool é melhor"
+                    }else{
+                        textViewResult.text = "Gasolina é melhor!"
+                    }
 
 
         }
