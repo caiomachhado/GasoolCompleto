b75fe4c98c627a1755df5e7fdb855b28d495596f
Caio Machado <caioamsul@gmail.com>
2021-04-13 19:42:09 -0300

    Correct 02

--- app/src/main/java/com/example/gasool_completo/MainActivity.kt
+++ app/src/main/java/com/example/gasool_completo/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.gasool_completo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.AppCompatTextView
 
+private const val ADVANTAGE_COEFFICIENT: Double = 0.7
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,22 +25,30 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-       private fun findViewByIds(){
-          buttonCalculate = findViewById(R.id.button_calculate)
-          editTextAlcohol = findViewById(R.id.edit_alcohol_value)
-          editTextOil = findViewById(R.id.edit_oil_value)
-          textViewResult = findViewById(R.id.text_view_result)
+    private fun findViewByIds() {
+        buttonCalculate = findViewById(R.id.button_calculate)
+        editTextAlcohol = findViewById(R.id.edit_alcohol_value)
+        editTextOil = findViewById(R.id.edit_oil_value)
+        textViewResult = findViewById(R.id.text_view_result)
     }
 
-    fun calculateBestFuel() {
+
+
+    private fun calculateBestFuel() {
         val alcoholPrice = editTextAlcohol?.text.toString()
         val oilPrice = editTextOil?.text.toString()
-        val result = alcoholPrice.toDouble() / oilPrice.toDouble()
-        if (result < 0.7) {
-            textViewResult?.text ?: "Álcool é melhor"
-        } else {
-            textViewResult?.text ?: "Gasolina é melhor!"
+        if(alcoholPrice.isNotEmpty() && oilPrice.isNotEmpty()){
+            val result = alcoholPrice.toDouble() / oilPrice.toDouble()
+            showResult(result)
+        }else{
+            Toast.makeText(applicationContext, "Campo em Branco!", Toast.LENGTH_LONG).show()
         }
-
     }
+
+    private fun showResult(result: Double) {
+        fun showResult(result: Double) {
+            var text = if (result < ADVANTAGE_COEFFICIENT) "Alcool é melhor" else "Gasolina é melhor"
+            textViewResult?.text = text
+    }
+
 }
