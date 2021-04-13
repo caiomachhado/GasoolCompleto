21428dbc95067ac48645d8ab05f884d260cf974d
Caio Machado <caioamsul@gmail.com>
2021-04-13 19:26:01 -0300

    Organized

--- app/src/main/java/com/example/gasool_completo/MainActivity.kt
+++ app/src/main/java/com/example/gasool_completo/MainActivity.kt
@@ -8,28 +8,36 @@ import androidx.appcompat.widget.AppCompatTextView
 
 
 class MainActivity : AppCompatActivity() {
+
+    private var buttonCalculate: AppCompatButton? = null
+    private var editTextAlcohol: AppCompatEditText? = null
+    private var editTextOil: AppCompatEditText? = null
+    private var textViewResult: AppCompatTextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewByIds()
+        buttonCalculate?.setOnClickListener {
+            calculateBestFuel()
+        }
+    }
 
+       private fun findViewByIds(){
+          buttonCalculate = findViewById(R.id.button_calculate)
+          editTextAlcohol = findViewById(R.id.edit_alcohol_value)
+          editTextOil = findViewById(R.id.edit_oil_value)
+          textViewResult = findViewById(R.id.text_view_result)
+    }
 
-        val buttonCalculate : AppCompatButton = findViewById(R.id.button_calculate)
-        val editTextAlcohol : AppCompatEditText = findViewById(R.id.edit_alcohol_value)
-        val editTextOil : AppCompatEditText = findViewById(R.id.edit_oil_value)
-        val textViewResult : AppCompatTextView = findViewById(R.id.text_view_result)
-
-
-                buttonCalculate.setOnClickListener{
-                    val alcoholPrice = editTextAlcohol.text.toString()
-                    val oilPrice = editTextOil.text.toString()
-                    val result = alcoholPrice.toDouble()/oilPrice.toDouble()
-                    if(result < 0.7){
-                        textViewResult.text = "Alcool é melhor"
-                    }else{
-                        textViewResult.text = "Gasolina é melhor!"
-                    }
-
-
+    fun calculateBestFuel() {
+        val alcoholPrice = editTextAlcohol.text.toString()
+        val oilPrice = editTextOil.text.toString()
+        val result = alcoholPrice.toDouble() / oilPrice.toDouble()
+        if (result < 0.7) {
+            textViewResult.text = "Álcool é melhor"
+        } else {
+            textViewResult.text = "Gasolina é melhor!"
         }
 
     }
